76ee03face0165c452d2553322436387d24e7152
Mike <matthews.r.mike@gmail.com>
2024-09-16 23:40:38 +0200

    Add UI settings

--- Settings.cs
+++ Settings.cs
@@ -1,17 +1,54 @@
 using UnityEngine;
 using UnityModManagerNet;
 
+using static DashboardUI.Dashboard;
 using static UnityModManagerNet.UnityModManager;
 
 namespace DashboardUI
 {
     public class Settings : ModSettings, IDrawable
     {
+        readonly static Color Brown = new Color(0.75f, 0.5f, 0.25f);
+
         // [Draw(DrawType.)]
-        
+
+        public enum ColorTag
+        {
+            White,
+            Grey,
+            Black,
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Magenta,
+            Cyan,
+            Brown
+        }
+
+        [Draw(DrawType.PopupList)]
+        public ColorTag primaryColor = ColorTag.White;
+        [Draw(DrawType.PopupList)]
+        public ColorTag secondaryColor = ColorTag.Cyan;
+        [Draw(DrawType.PopupList)]
+        public ColorTag pointerColor = ColorTag.Red;
+        [Space]
+        [Draw(DrawType.PopupList)]
+        public Orientation orientation;
+        [Draw(DrawType.Slider, Min = -1, Max = 1, Precision = 3)]
+        public float xPositionPercent;
+        [Draw(DrawType.Slider, Min = 0, Max = 1, Precision = 3)]
+        public float yPositionPercent;
+        [Draw(DrawType.Slider, Min = 0.5f, Max = 2, Precision = 3)]
+        public float uiScale;
+
+        // TODO : Add default values here
+
         [Header("Debug")]
         [Draw(DrawType.Toggle)]
-        public bool showMarkers;
+        public bool showMarkers = false;
+        [Draw(DrawType.Toggle)]
+        public bool disableInfoLogs = true;
 
         public override void Save(ModEntry modEntry) => Save(this, modEntry);
 
@@ -20,6 +57,35 @@ namespace DashboardUI
             Main.SetMarkers(showMarkers);
 
             //
+        }
+
+        public static Color GetColor(ColorTag tag)
+        {
+            switch (tag)
+            {
+                case ColorTag.White:
+                    return Color.white;
+                case ColorTag.Grey:
+                    return Color.grey;
+                case ColorTag.Black:
+                    return Color.black;
+                case ColorTag.Red:
+                    return Color.red;
+                case ColorTag.Green:
+                    return Color.green;
+                case ColorTag.Blue:
+                    return Color.blue;
+                case ColorTag.Yellow:
+                    return Color.yellow;
+                case ColorTag.Magenta:
+                    return Color.magenta;
+                case ColorTag.Cyan:
+                    return Color.cyan;
+                case ColorTag.Brown:
+                    return Brown;
+                default:
+                    return Color.clear;
+            }
         }
     }
 }
