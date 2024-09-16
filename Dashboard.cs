486eb0576aafacf365945d00102a197671401714
Mike <matthews.r.mike@gmail.com>
2024-09-16 20:04:10 +0200

    Copy init method from DamageUI

--- Dashboard.cs
+++ Dashboard.cs
@@ -1,32 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Reflection;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace DashboardUI
 {
     class Dashboard : MonoBehaviour
     {
+        static Dashboard instance;
+
         Image rev;
         Image limiter;
         Image tickPrefab;
         Text gear;
         Text speed;
         Text unit;
+        CanvasGroup group;
 
-        bool initialized = false;
+        void Awake() => StartCoroutine(InitWhenReady());
 
-        // TODO : Get components refs
-        // TODO : Copy init system from Damage UI
+        IEnumerator InitWhenReady()
+        {
+            // this is here so we don't have the glitch
+            group = gameObject.AddComponent<CanvasGroup>();
+            group.alpha = 0;
+            group.blocksRaycasts = false;
+
+            // check manager availability
+            GameEntryPoint entry = FindObjectOfType<GameEntryPoint>();
+
+            if (entry == null)
+            {
+                Main.Log("Can't find entry point. Are you sure you're spawning the UI in the right scene ?");
+                yield break;
+            }
+
+            FieldInfo testInfo = entry.GetType().GetField("eventManager", BindingFlags.Static | BindingFlags.NonPublic);
+            yield return new WaitUntil(() => testInfo.GetValue(entry) != null);
+
+            // init
+            // refresh values
+        }
 
         void Init()
         {
-            //rev = transform.GetChild(0).GetChild(1).GetComponentInChildren<Image>();
+            Main.Try(() =>
+            {
+                instance = this;
 
-            initialized = true;
+                // TODO : Get components refs
+                // rev = transform.GetChild(0).GetChild(1).GetComponentInChildren<Image>();
+            });
+        }
+
+        void LateUpdate()
+        {
+            if (instance == null)
+                return;
+
+            //
         }
 
         // TODO : Add method to set colors
-        // TODO : Add method to update rev
-        // TODO : Add method to update speed
         // TODO : Add method to update units
+        // TODO : Add method to update gear
     }
 }
