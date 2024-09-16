ce688bb5367b7e897f9691b822f57f4ef028c46e
Mike <matthews.r.mike@gmail.com>
2024-09-16 21:39:28 +0200

    Update todos

--- Patches.cs
+++ Patches.cs
@@ -2,28 +2,29 @@ using HarmonyLib;
 
 namespace DashboardUI
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // [HarmonyPatch(typeof(), MethodType.)]
+    // static class type_method_Patch
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
 
-	// TODO : Make assets for the dashboard in Unity
-	// TODO : Export asset bundles
-	// TODO : Load asset bundle
-	// TODO : Spawn UI on start of game
-	// TODO : Plug values into dashboard UI
-	// TODO : Hide game's dahsboard UI
-	// TODO : Plug into menu animations
-	// TODO : Add positionning and scale settings
+    // TODO : Make Left dashboard asset in Unity
+    // TODO : Make Center dashboard asset in Unity
+    // TODO : Export asset bundles
+    // TODO : Load asset bundle
+    // TODO : Spawn UI on start of game
+    // TODO : Plug values into dashboard UI
+    // TODO : Hide game's dahsboard UI
+    // TODO : Plug into menu animations
+    // TODO : Add positionning and scale settings
 }
