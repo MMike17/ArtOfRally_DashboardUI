fc131028d06e66723b6ab79d74517aa432067b1a
Mike <matthews.r.mike@gmail.com>
2024-09-16 21:33:10 +0200

    Add ref get on monobehaviour and setup ticks and limiter

--- Dashboard.cs
+++ Dashboard.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,19 +9,36 @@ namespace DashboardUI
 {
     class Dashboard : MonoBehaviour
     {
+        const float MIN_ANGLE = 45;
+        const float MAX_ANGLE = -135;
+
         static Dashboard instance;
 
         Image rev;
         Image limiter;
-        Image tickPrefab;
+        Transform tickHolder;
+        Tick tickPrefab;
+        Transform dial;
         Text gear;
-        Text speed;
         Text unit;
+        Text speed;
         CanvasGroup group;
 
-        void Awake() => StartCoroutine(InitWhenReady());
+        List<Tick> ticks;
+        Vector2Int revThresholds;
+        Func<int> GetSpeed;
+        Func<int> GetRev;
 
-        IEnumerator InitWhenReady()
+        public void Init(Vector2Int revThresholds, Func<int> GetSpeed, Func<int> GetRev, string units)
+        {
+            this.revThresholds = revThresholds;
+            this.GetSpeed = GetSpeed;
+            this.GetRev = GetRev;
+
+            StartCoroutine(InitWhenReady(units));
+        }
+
+        IEnumerator InitWhenReady(string units)
         {
             // this is here so we don't have the glitch
             group = gameObject.AddComponent<CanvasGroup>();
@@ -38,18 +57,39 @@ namespace DashboardUI
             FieldInfo testInfo = entry.GetType().GetField("eventManager", BindingFlags.Static | BindingFlags.NonPublic);
             yield return new WaitUntil(() => testInfo.GetValue(entry) != null);
 
-            // init
+            Init(units);
             // refresh values
         }
 
-        void Init()
+        void Init(string units)
         {
             Main.Try(() =>
             {
                 instance = this;
 
-                // TODO : Get components refs
-                // rev = transform.GetChild(0).GetChild(1).GetComponentInChildren<Image>();
+                rev = transform.GetChild(0).GetChild(1).GetComponentInChildren<Image>();
+                limiter = transform.GetChild(0).GetChild(2).GetComponentInChildren<Image>();
+
+                tickHolder = transform.GetChild(0).GetChild(3);
+                tickPrefab = new Tick(tickHolder.GetChild(0), 0);
+
+                dial = transform.GetChild(0).GetChild(4);
+                gear = transform.GetChild(1).GetComponent<Text>();
+                unit = transform.GetChild(2).GetComponent<Text>();
+                unit.text = units;
+                speed = transform.GetChild(3).GetComponent<Text>();
+
+                ticks = new List<Tick>();
+                int ticksCount = (revThresholds.y - revThresholds.x) / 1000;
+
+                for (int i = 0; i <= ticksCount; i++)
+                {
+                    Tick tick = new Tick(Instantiate(tickPrefab.transform, tickHolder), i + 1);
+                    tick.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Lerp(MIN_ANGLE, MAX_ANGLE, (float)i / ticksCount));
+                    ticks.Add(tick);
+                }
+
+                limiter.fillAmount = (float)1 / ticksCount;
             });
         }
 
@@ -64,5 +104,17 @@ namespace DashboardUI
         // TODO : Add method to set colors
         // TODO : Add method to update units
         // TODO : Add method to update gear
+
+        class Tick
+        {
+            public Transform transform;
+
+            public Tick(Transform transform, int level)
+            {
+                this.transform = transform;
+
+                transform.GetComponentInChildren<Text>().text = level.ToString();
+            }
+        }
     }
 }
